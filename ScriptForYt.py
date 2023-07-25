b8d46f70641ff30062d151744c306106efd6427e
Nirbhay Singh <90407590+NiRbHaYsInGh30@users.noreply.github.com>
2023-07-25 16:39:27 +0530

    Done

---
+++ ScriptForYt.py
@@ -0,0 +1,17 @@
+# install pytube first Using pip install pytube command
+from pytube import YouTube
+from sys import argv
+
+# command line arg passed
+link = argv[1]
+yt = YouTube(link)
+
+# for printing views and title
+print("Title: ", yt.title)
+print("View: ", yt.views)
+
+# for getting the highest resolution
+yd = yt.streams.get_highest_resolution()
+
+# ADD FOLDER HERE
+yd.download('D:\Games')
